074038c5df0e09783a97638dde4011dff76bc93e
tomazpecoraro <tomazpecoraro0@gmail.com>
2023-11-17 09:47:35 -0300

    CSS Home

--- src/components/cabecalho/cabecalho.jsx
+++ src/components/cabecalho/cabecalho.jsx
@@ -5,18 +5,18 @@ export default function Cabecalho() {
   return (
     <header className={styles.header}>
       <nav>
-        <div class={styles.cabecalho}>
-          <h1 class={styles.NOME}>
-            <Link href="/">NOME</Link>
+        <div className={styles.cabecalho}>
+          <h1 className={styles.NOME}>
+            <Link href="/" className = {styles.link}>NOME</Link>
           </h1>
           <div className={styles.botao}>
-            <Link href="../../pages/doencas/doencaspage">Doenças de Pele</Link>
+            <Link href="../../pages/doencas/doencaspage" className = {styles.link}>Doenças de Pele</Link>
           </div>
           <div className={styles.botao}>
-            <Link href="../../pages/formulario/formulariopage">Formulário</Link>
+            <Link href="../../pages/formulario/formulariopage" className = {styles.link}>Formulário</Link>
           </div>
           <div className={styles.botao}>
-            <Link href="../../pages/membros/membrospage">Membros</Link>
+            <Link href="../../pages/membros/membrospage" className = {styles.link}>Membros</Link>
           </div>
           <div className={styles.botao}>
             <Link href="../../pages/login/loginpage">Login</Link>
