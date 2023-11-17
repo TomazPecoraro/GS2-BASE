b98f497f35c5122763795a5f73c395c88fc5da1a
tomazpecoraro <tomazpecoraro0@gmail.com>
2023-11-17 08:49:10 -0300

    Página Home e Cabeçalho

---
+++ src/app/layout.jsx
@@ -0,0 +1,24 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+import { Montserrat } from 'next/font/google'
+import Cabecalho from '@/components/Cabecalho/cabecalho'
+import Rodape from '@/components/rodape/rodape'
+
+const monteserrat = Montserrat({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'GS-2',
+}
+
+export default function RootLayout({children}) {
+  return (
+    <html lang="pt-br">
+      <body>
+        <Cabecalho/>
+        {children}
+        <Rodape/>
+      </body>
+    </html>
+  )
+}
