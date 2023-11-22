4ebbc5aa62688feaafb6121fd7dd850ed446cc8b
tomazpecoraro <tomazpecoraro0@gmail.com>
2023-11-21 22:53:50 -0300

    Tela Cadastro

--- src/pages/cadastro/page.jsx
+++ src/pages/cadastro/page.jsx
@@ -1,15 +1,16 @@
 import '../../app/globals.css'
 import Header from '../../components/cabecalho/cabecalho';
 import Rodape from '../../components/rodape/rodape';
+import Cadastro from '../../components/cadastro/cadastro'
 
-export default function Login() {
+export default function PGCadastro() {
     return (
     <div>
         <header>
             <Header/>
         </header>
     <section>
-
+        <Cadastro/>
     </section>
         <footer>
             <Rodape/>
