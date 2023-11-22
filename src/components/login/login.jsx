70932b6081b1960d31607b5c54afea7ddb698fa4
tomazpecoraro <tomazpecoraro0@gmail.com>
2023-11-21 22:48:07 -0300

    Tela Login

---
+++ src/components/login/login.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import styles from './login.module.css'
+import Link from "next/link";
+
+export default function Login() {
+
+        const [usuario, setUsuario] = useState({
+          info: "login",
+          email: "",
+          senha: ""
+        });
+
+        const handleChange = (e) =>{
+          const{name, value} = e.target;
+          setUsuario({...usuario,[name]:value});
+          };
+
+          const handleSubmit = async (e)=>{
+            e.preventDefault();
+
+            let user;
+
+            try {
+              const response = await fetch("http://localhost:3000/api/base/base-users",{
+                method:"POST",
+                headers:{
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(usuario),
+              });
+
+              if(response.ok){
+                const data = response.json();
+
+                if(data){
+                  alert("LOGIN REALIZADO COM SUCESSO!")
+                }else{
+                  alert("SENHA E OU USUAÁRIOS INVÁLIDOS!")
+                }
+
+              }
+
+          }catch(error){
+            console.error(error);
+          }
+        }
+  return (
+    <div className={styles.bg}>
+        <div className={styles.login}>
+        <h1 className={styles.titulologin}>Login</h1>
+        <div>
+            <form onSubmit={handleSubmit}>
+            <fieldset>
+                <div>
+                    <input className={styles.campos} type="email" name="email" id="idEmail" placeholder="Usuário" value={usuario.email} onChange={handleChange}/>
+                </div>
+                <div>
+                 <input className={styles.campos} type="password" name="senha" id="idSenha" placeholder="Senha" value={usuario.senha} onChange={handleChange}/>
+                </div>
+                <div>
+                    <Link className={styles.linkcadastro} href='../cadastro/page'>Quero me Cadastrar</Link>
+                </div>
+                <div>
+                    <button className={styles.botao}>Entrar</button>
+                </div>
+            </fieldset>
+            </form>
+        </div>
+        </div>
+    </div>
+  );
+}
--- src/pages/login/page.jsx
+++ src/pages/login/page.jsx
@@ -1,15 +1,16 @@
 import '../../app/globals.css'
 import Header from '../../components/cabecalho/cabecalho';
 import Rodape from '../../components/rodape/rodape';
+import Login from '../../components/login/login'
 
-export default function Login() {
+export default function PGLogin() {
     return (
     <div>
         <header>
             <Header/>
         </header>
     <section>
-
+        <Login/>
     </section>
         <footer>
             <Rodape/>
